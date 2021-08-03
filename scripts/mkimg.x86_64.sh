79a69d9ed279723f5f588eae357b26d8d5b15208
Robin Röper <rr@linux.com>
2021-08-03 10:07:08 +0200

    Update mkimg.x86_64.sh

--- scripts/mkimg.x86_64.sh
+++ scripts/mkimg.x86_64.sh
@@ -27,7 +27,7 @@ profile_x86_64() {
 		wireless-tools wpa_supplicant zonenotify
 		btrfs-progs cksfv dosfstools cryptsetup
 		cciss_vol_status e2fsprogs e2fsprogs-extra efibootmgr
-		grub-bios grub-efi lvm2 mdadm mkinitfs mtools nfs-utils
+		grub-bios grub-efi lvm2 mdadm mtools nfs-utils
 		parted rsync sfdisk syslinux unrar util-linux xfsprogs
 		zfs
 		"
